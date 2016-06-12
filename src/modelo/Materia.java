68820a87fa7efaae19cb04a20a6ec716dfcf43a6
Santiago Yépez <santymatarata@gmail.com>
2016-06-11 20:56:05 -0500

    Con JPA.

---
+++ src/modelo/Materia.java
@@ -0,0 +1,81 @@
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the materia database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Materia.findAll", query="SELECT m FROM Materia m")
+public class Materia implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idMateria;
+
+	private String materia;
+
+	//bi-directional many-to-one association to Categoria
+	@OneToMany(mappedBy="materia")
+	private List<Categoria> categorias;
+
+	//bi-directional many-to-one association to Carrera
+	@ManyToOne
+	@JoinColumn(name="IdCarrera")
+	private Carrera carrera;
+
+	public Materia() {
+	}
+
+	public int getIdMateria() {
+		return this.idMateria;
+	}
+
+	public void setIdMateria(int idMateria) {
+		this.idMateria = idMateria;
+	}
+
+	public String getMateria() {
+		return this.materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+	public List<Categoria> getCategorias() {
+		return this.categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public Categoria addCategoria(Categoria categoria) {
+		getCategorias().add(categoria);
+		categoria.setMateria(this);
+
+		return categoria;
+	}
+
+	public Categoria removeCategoria(Categoria categoria) {
+		getCategorias().remove(categoria);
+		categoria.setMateria(null);
+
+		return categoria;
+	}
+
+	public Carrera getCarrera() {
+		return this.carrera;
+	}
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+
+}
