298380ca33d022b26814acf5242de79a52a2dd59
Santiago Yépez <santymatarata@gmail.com>
2016-08-03 15:13:20 -0500

    Consulta usuarios por alias.

--- src/modelo/operacion/TablaUsuario.java
+++ src/modelo/operacion/TablaUsuario.java
@@ -1,8 +1,11 @@
 package modelo.operacion;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import model.Usuario;
 
@@ -16,9 +19,17 @@ public class TablaUsuario {
 			em.persist(u);
 			em.getTransaction().commit();
 			em.close();
-			} catch (Exception e) {
-				ret = false;
-			}
+		} catch (Exception e) {
+			ret = false;
+		}
+		return ret;
+	}
+	
+	public static List<Usuario> consultarPorAlias(String alias){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PruebaJPA");
+		EntityManager em = emf.createEntityManager();
+		TypedQuery<Usuario> query = em.createQuery("SELECT u FROM Usuario u WHERE alias LIKE '%" + alias + "%'", Usuario.class);
+		List<Usuario> ret = query.getResultList();
 		return ret;
 	}
 }
--- src/rest/SrvUsuario.java
+++ src/rest/SrvUsuario.java
@@ -1,6 +1,7 @@
 package rest;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import javax.ws.rs.*;
 
@@ -11,19 +12,28 @@ import modelo.operacion.TablaUsuario;
 @Produces("application/json")
 public class SrvUsuario {
 
-	@GET
-	/*@Path("/registrar")
-	@Consumes("application/json")*/
+	
+	/*@GET
 	@Path("/registrar/{nombre}/{apellido}/{alias}/{clave}")
-	public String registrar(@PathParam("nombre") String nombre, @PathParam("apellido") String apellido, @PathParam("alias") String alias, @PathParam("clave") String clave){
+	public String registrar(@PathParam("nombre") String nombre, @PathParam("apellido") String apellido, @PathParam("alias") String alias, @PathParam("clave") String clave){}*/
+	@POST
+	@Path("/registrar")
+	@Consumes("application/json")
+	public String registrar(Usuario u){
 		String ret = "registro correcto";
-		Usuario u = new Usuario();
+		/*Usuario u = new Usuario();
 		u.setAlias(alias);
 		u.setNombre(nombre);
 		u.setContraseña(clave);
 		u.setApellido(apellido);
-		u.setFechaRegistro(new GregorianCalendar().getTime());
+		u.setFechaRegistro(new GregorianCalendar().getTime());*/
 		if (!TablaUsuario.registrar(u)) ret = "error registro";
 		return ret;
+	}
+	
+	@GET
+	@Path("/consultar/{alias}")
+	public List<Usuario> consultar(@PathParam("alias") String alias){
+		return TablaUsuario.consultarPorAlias(alias);
 	}
 }
