6672184169e7893efc543c9bbf3388a0cbbd3a8d
Santiago Yépez <santymatarata@gmail.com>
2016-08-03 20:32:04 -0500

    Algunas cosas tontas.

--- src/modelo/operacion/Operador.java
+++ src/modelo/operacion/Operador.java
@@ -5,11 +5,12 @@ import java.sql.*;
 public class Operador {
 	
 	private Connection con;
-	private String cadConexionMySQL,cadConexionMySQLRemoto;
+	private String cadConexionMySQL, cadConexionMySQLRemoto, cadConexionMySQL4Free;
 	
 	public Operador() {
 		cadConexionMySQL = "jdbc:mysql://localhost/debint?user=root&password=1234";
 		cadConexionMySQLRemoto = "jdbc:mysql://199.168.189.114/integrae_appweb?user=integrae_userApp&password=AplicacionesWeb2016";
+		cadConexionMySQL4Free = "jdbc:mysql://db4free.net/debint?user=appsweb&password=appsweb";
 	}
 	
 	public void conectar() throws SQLException {
